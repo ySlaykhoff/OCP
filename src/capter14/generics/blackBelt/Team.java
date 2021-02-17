4be5259ac918f7ce837378bb16d6a2d817df6d39
ySlyakhoff <slyakhoff@yandex.ru>
2021-02-18 00:05:50 +0300

    add Team class Constructor

--- src/capter14/generics/blackBelt/Team.java
+++ src/capter14/generics/blackBelt/Team.java
@@ -6,4 +6,8 @@ import java.util.List;
 public class Team {
     private String name;
     private List<Person> persons= new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
 }
