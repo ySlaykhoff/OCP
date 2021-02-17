7e5dd2775bee288784be20650cbe2ce635ad1f8c
ySlyakhoff <slyakhoff@yandex.ru>
2021-02-18 00:04:51 +0300

    add Team class

---
+++ src/capter14/generics/blackBelt/Team.java
@@ -0,0 +1,9 @@
+package capter14.generics.blackBelt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Person> persons= new ArrayList<>();
+}
