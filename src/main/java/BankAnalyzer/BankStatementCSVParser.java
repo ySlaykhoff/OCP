ceb6bedca01b94f1ae85de6ef249425006508536
ySlyakhoff <slyakhoff@yandex.ru>
2022-01-12 11:52:44 +0300

    class Team aplay genrics step two

--- src/main/java/BankAnalyzer/BankStatementCSVParser.java
+++ src/main/java/BankAnalyzer/BankStatementCSVParser.java
@@ -1,5 +1,29 @@
 package BankAnalyzer;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BankStatementCSVParser {
 
+    public static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    private BankTransaction parseFromCSV(String line){
+        String[] columns = line.split(",");
+        LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+        double amount = Double.parseDouble(columns[1]);
+        String description = columns[2];
+
+        return new BankTransaction(date,amount,description);
+    }
+
+    public List<BankTransaction> parseLinesFromCSV(List<String> lines){
+        List<BankTransaction> bankTransactions = new ArrayList<>();
+
+        for (String line: lines){
+            bankTransactions.add(parseFromCSV(line));
+        }
+       return bankTransactions;
+    }
 }
