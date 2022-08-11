278176a40463358a59c89a0b9ddb38018e218baf
ySlyakhoff <slyakhoff@yandex.ru>
2022-08-11 18:42:21 +0300

    class Team aplay genrics step two

--- src/main/java/LeoPractic/CH1/TestCH1.java
+++ src/main/java/LeoPractic/CH1/TestCH1.java
@@ -1,21 +1,20 @@
 package LeoPractic.CH1;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class TestCH1 {
     public static void main(String[] args) {
+        String str = String.valueOf(Character.toChars (128149));
         Map<Character,Integer> countDuplicate = countDuplicateCharacters("Hello world!");
         System.out.println(countDuplicate);
+        Map<String, Long> countDuplicate2 = countDuplicateCharactersStreamU32("Hello world\uD83D\uDC95!"+str);
+        System.out.println(countDuplicate2);
         Map<Character, Long> result = countDuplicateCharactersStream("Hello world!");
         System.out.println(result);
+
+        System.out.println(str);
     }
 
     public static Map<Character, Integer> countDuplicateCharacters(String strLine) {
@@ -28,6 +27,12 @@ public class TestCH1 {
 
     public static Map<Character, Long> countDuplicateCharactersStream(String strLine) {
         Map<Character, Long> result = strLine.chars().mapToObj(c -> (char)c).collect(Collectors.groupingBy(c -> c,Collectors.counting()));
+        return result;
+    }
+
+    public static Map<String, Long> countDuplicateCharactersStreamU32(String str) {
+        Map<String, Long> result = str.codePoints()
+                .mapToObj (c -> String.valueOf(Character.toChars (c))) .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
         return result;
     }
 }
